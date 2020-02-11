5a22da8c633d2bc3da627f245df6b7bdbe96e6f2
dkam26 <gkam1989@gmail.com>
2020-02-11 18:40:21 +0300

    Connect React to apollo

--- shoppinglist/src/component/Shoppinglists.js
+++ shoppinglist/src/component/Shoppinglists.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 const SHOPPINGLISTS = gql`
   {
     shoppinglists{
@@ -8,16 +12,24 @@ const SHOPPINGLISTS = gql`
     }
   }
 `;
+const useStyles = makeStyles(theme => ({
+    root: {
+      width: '100%',
+      maxWidth: 360,
+      backgroundColor: theme.palette.background.paper,
+      marginLeft: 459,
+      marginTop: 100,
+    },
+  }));
 export default function Shoppinglists() {
+    const classes = useStyles();
     const { loading, error, data } = useQuery(SHOPPINGLISTS);
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error :(</p>;
-    return data.shoppinglists.map(({ name }) => (
-        <div key={name}>
-          <p>
-            {name}
-          </p>
-        </div>
-      ));;
+        return data.shoppinglists.map(({ name }) => (
+            <ListItem button>
+                <ListItemText inset primary={name} />
+            </ListItem>
+        ));
   }
